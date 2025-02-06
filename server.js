46d0bf84a60bdfe93a12c33aad318700aebd9837
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2025-02-06 09:49:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT
 
 // JSON file to store landing page data
 const DATA_FILE = "./landingPages.json";
