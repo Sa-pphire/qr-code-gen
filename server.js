be2d7f3d6d114616dcfc06829287aef635c4e546
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2025-02-20 18:41:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,8 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
-console.log(process.env)
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 // Define Schema for QR Code Data
 const QRCodeSchema = new mongoose.Schema({
   title: String,
