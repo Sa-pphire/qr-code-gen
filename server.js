18a4c06e4ac25564e4fc8cb8b1b5405ff6c6eb7f
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2025-02-20 18:35:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
-console.log(process.env.MONGO_URI)
+console.log(process.env)
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
